cb77082708aadc7653a325bd49aa5b6b323dce8e
a-oselkov <a.oselkov@gmail.com>
2023-03-15 22:16:49 +0300

    add StringSchema

---
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -0,0 +1,24 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class StringSchemaTest {
+
+    @Test
+    void required() {
+    }
+
+    @Test
+    void minLength() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void isValid() {
+    }
+}
